a92811ef4ca208af952994f2b26c7794debce777
Gavin Swett <gavin.swett@ntrprise.net>
2025-12-04 20:34:52 -0700

    Refactoring with YAGSL Swerve Example

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,37 +4,48 @@
 
 package frc.robot;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkRelativeEncoder;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
+ * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
  */
-public final class Constants {
+public final class Constants
+{
 
-  public static final class DrivebaseConstants {
-    public static final SparkMax frontLeftMotor = new SparkMax(3, MotorType.kBrushless);
-    public static final SparkMax frontRightMotor = new SparkMax(4, MotorType.kBrushless);
-    public static final SparkMax rearLeftMotor = new SparkMax(5, MotorType.kBrushless);
-    public static final SparkMax rearRightMotor = new SparkMax(7, MotorType.kBrushless);
+  public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
+  public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+  public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
+  public static final double MAX_SPEED  = Units.feetToMeters(14.5);
+  // Maximum speed of the robot in meters per second, used to limit acceleration.
+
+//  public static final class AutonConstants
+//  {
+//
+//    public static final PIDConstants TRANSLATION_PID = new PIDConstants(0.7, 0, 0);
+//    public static final PIDConstants ANGLE_PID       = new PIDConstants(0.4, 0, 0.01);
+//  }
+
+  public static final class DrivebaseConstants
+  {
+
+    // Hold time on motor brakes when disabled
+    public static final double WHEEL_LOCK_TIME = 10; // seconds
   }
 
-  public static final class PuckShooterConstants {
-    public static final SparkMax leftShooter = new SparkMax(2, MotorType.kBrushless);
-    public static final SparkMax rightShooter = new SparkMax(6, MotorType.kBrushless);
+  public static class OperatorConstants
+  {
 
-    public static final SparkMax spinner = new SparkMax(1, MotorType.kBrushed);
-    public static final double spinnerSpeed = 0.25;
-    public static final double shooterSpeed = 1.0;
-
-    public static final SparkRelativeEncoder leftShooterEncoder = (SparkRelativeEncoder) leftShooter.getEncoder();
-    public static final SparkRelativeEncoder rightShooterEncoder = (SparkRelativeEncoder) rightShooter.getEncoder();
-  }
-
-  public static class OperatorConstants {
-    public static final int driverControllerPort = 0;
+    // Joystick Deadband
+    public static final double DEADBAND        = 0.1;
+    public static final double LEFT_Y_DEADBAND = 0.1;
+    public static final double RIGHT_X_DEADBAND = 0.1;
+    public static final double TURN_CONSTANT    = 6;
   }
 }
