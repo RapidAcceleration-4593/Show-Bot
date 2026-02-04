1678482555094c85decc8d505eea5c9875b4fb91
Michael Schnee <maschnee11@icloud.com>
2026-02-03 18:59:53 -0700

    Began to add shooter subsystem and commands

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.shooter;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import frc.robot.Constants;
+
+public class ShooterSubsystem {
+    SparkMax spinnerA;
+    SparkMax spinnerB;
+    SparkMax feeder;
+    public ShooterSubsystem(){
+        spinnerA = new SparkMax(Constants.ShooterConstants.SHOOT_SPINNER_A_ID, MotorType.kBrushless);
+        spinnerB = new SparkMax(Constants.ShooterConstants.SHOOT_SPINNER_B_ID, MotorType.kBrushless);
+        feeder = new SparkMax(Constants.ShooterConstants.SHOOT_FEEDER_ID, MotorType.kBrushless);
+        SparkMaxConfig bConfig = new SparkMaxConfig();
+        bConfig.follow(spinnerA, true);
+        spinnerB.configure(bConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    }
+    public void startSpinner(){
+        spinnerA.setVoltage(12.0);
+    }
+    public void startFeeder(){
+        feeder.setVoltage(3.0);
+    }
+}
--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -47,10 +47,10 @@ public class SwerveSubsystem extends SubsystemBase
    * Swerve drive object.
    */
   private final SwerveDrive swerveDrive;
+  
   /**
    * Enable vision odometry updates while driving.
    */
-  
   private final boolean     visionDriveTest = false;
 
   /**
