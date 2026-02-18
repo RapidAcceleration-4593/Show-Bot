bba10b8fbf712869cc29c9b996b39457cc90fa4c
Michael Schnee <maschnee11@icloud.com>
2026-02-17 20:56:57 -0700

    Removed Rev Logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import com.revrobotics.util.StatusLogger;
+
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
@@ -41,6 +43,7 @@ public class Robot extends TimedRobot
   @Override
   public void robotInit()
   {
+    StatusLogger.disableAutoLogging();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
