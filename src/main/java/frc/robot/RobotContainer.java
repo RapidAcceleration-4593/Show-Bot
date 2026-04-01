40fadbe9dce94a3b0ded8c67753e3f9de1016982
Matthew Flanegan <mvoncrackersnackin@gmail.com>
2026-03-31 20:39:14 -0600

    Switched to absolute encoders, enabled swerve drive in RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,19 +4,11 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.shooter.ShootCommand;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
@@ -30,22 +22,22 @@ public class RobotContainer {
 	// Replace with CommandPS4Controller or CommandJoystick if needed
 	final         CommandXboxController driverXbox = new CommandXboxController(0);
 	// The robot's subsystems and commands are defined here...
-	// private final SwerveSubsystem       drivebase  = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+	private final SwerveSubsystem       drivebase  = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
 	private final ShooterSubsystem shooter = new ShooterSubsystem();
 	private final LEDSubsystem leds = new LEDSubsystem();
-	// SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
-	// 																() -> driverXbox.getLeftY(),
-	// 																() -> driverXbox.getLeftX() * -1)
-	// 															.withControllerRotationAxis(() -> driverXbox.getRightX() * -1)
-	// 															.deadband(OperatorConstants.DEADBAND)
-	// 															.scaleTranslation(0.8)
-	// 															.allianceRelativeControl(true);
+	SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
+																	() -> driverXbox.getLeftY(),
+																	() -> driverXbox.getLeftX() * -1)
+																.withControllerRotationAxis(() -> driverXbox.getRightX() * -1)
+																.deadband(OperatorConstants.DEADBAND)
+																.scaleTranslation(0.8)
+																.allianceRelativeControl(true);
 
-	// SwerveInputStream driveRobotOriented = driveAngularVelocity.copy().robotRelative(true)
-																// .allianceRelativeControl(false);
+	SwerveInputStream driveRobotOriented = driveAngularVelocity.copy().robotRelative(true)
+																.allianceRelativeControl(false);
 
-	// Command driveFieldOrientedAngularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
-	// Command driveRobotOrientedAngularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
+	Command driveFieldOrientedAngularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
+	Command driveRobotOrientedAngularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
 
 	public RobotContainer() {
 		// Configure the trigger bindings
@@ -54,9 +46,9 @@ public class RobotContainer {
 	}
 
 	private void configureBindings() {
-		// drivebase.setDefaultCommand(driveFieldOrientedAngularVelocity);
+		drivebase.setDefaultCommand(driveFieldOrientedAngularVelocity);
 
-		// driverXbox.a().onTrue((Commands.runOnce(drivebase::zeroGyro)));
+		driverXbox.a().onTrue((Commands.runOnce(drivebase::zeroGyro)));
 		driverXbox.rightTrigger().onTrue(new ShootCommand(shooter));
 
 		driverXbox.b().onTrue(leds.nextPatternCommand());
@@ -68,6 +60,6 @@ public class RobotContainer {
 	}
 
 	public void setMotorBrake(boolean brake) {
-		// drivebase.setMotorBrake(brake);
+		drivebase.setMotorBrake(brake);
 	}
 }
